43898cdcce11ff32ef3132cb8e0738e8f9920067
Alex Dillhoff <orbv16@gmail.com>
2014-06-24 14:46:34 -0500

    Cleaned up debugging code

--- PvPHelper.lua
+++ PvPHelper.lua
@@ -2,10 +2,10 @@
 -- Client Lua Script for PvPHelper
 -- by orbv - Bloodsworn - Dominion
 -----------------------------------------------------------------------------------------------
- 
+
 require "Window"
 require "Apollo"
- 
+
 -----------------------------------------------------------------------------------------------
 -- PvPHelper Module Definition
 -----------------------------------------------------------------------------------------------
@@ -14,22 +14,23 @@ local PvPHelper = {
 	pvphelperdb, 
 	currentMatch
 } 
- 
+
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
--- e.g. local kiExampleVariableMax = 999
 
+-- TODO: ktPvPEvents may not be needed
 local ktPvPEvents =
 {
-	[PublicEvent.PublicEventType_PVP_Arena] 					= true,
-	[PublicEvent.PublicEventType_PVP_Warplot] 					= true,
-	[PublicEvent.PublicEventType_PVP_Battleground_Vortex] 		= true,
-	[PublicEvent.PublicEventType_PVP_Battleground_Cannon] 		= true,
-	[PublicEvent.PublicEventType_PVP_Battleground_Sabotage]		= true,
+	[PublicEvent.PublicEventType_PVP_Arena] 					         	= true,
+	[PublicEvent.PublicEventType_PVP_Warplot] 									= true,
+	[PublicEvent.PublicEventType_PVP_Battleground_Vortex] 			= true,
+	[PublicEvent.PublicEventType_PVP_Battleground_Cannon] 			= true,
+	[PublicEvent.PublicEventType_PVP_Battleground_Sabotage]			= true,
 	[PublicEvent.PublicEventType_PVP_Battleground_HoldTheLine] 	= true,
 }
 
+-- TODO: This will be expanded to a table if more views are added
 local kEventTypeToWindowName = "ResultGrid"
 
 local tDataKeys = {
@@ -38,18 +39,18 @@ local tDataKeys = {
 	"sResult",
 	"sRating",
 }
- 
+
 -----------------------------------------------------------------------------------------------
 -- Initialization
 -----------------------------------------------------------------------------------------------
 function PvPHelper:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self 
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self 
 
-    -- initialize variables here
+  -- initialize variables here
 
-    return o
+  return o
 end
 
 function PvPHelper:Init()
@@ -58,25 +59,25 @@ function PvPHelper:Init()
 	local tDependencies = {
 		--"Gemini:Logging-1.2",
 	}
-    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
+	Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
 
 	self.db = Apollo.GetPackage("Gemini:DB-1.0").tPackage:New(self)
 end
- 
+
 
 -----------------------------------------------------------------------------------------------
 -- PvPHelper OnLoad
 -----------------------------------------------------------------------------------------------
 function PvPHelper:OnLoad()
-    -- load our form file
-	self.xmlDoc = XmlDoc.CreateFromFile("PvPHelper.xml")
-	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
-	
-	if self.db.char.PvPHelper == nil then
-		self.db.char.PvPHelper = {}
-	end
-	
-	self.pvphelperdb = self.db.char.PvPHelper
+  -- load our form file
+  self.xmlDoc = XmlDoc.CreateFromFile("PvPHelper.xml")
+  self.xmlDoc:RegisterCallback("OnDocLoaded", self)
+
+  if self.db.char.PvPHelper == nil then
+  	self.db.char.PvPHelper = {}
+  end
+
+  self.pvphelperdb = self.db.char.PvPHelper
 end
 
 -----------------------------------------------------------------------------------------------
@@ -85,13 +86,13 @@ end
 function PvPHelper:OnDocLoaded()
 
 	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
-	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "PvPHelperForm", nil, self)
+		self.wndMain = Apollo.LoadForm(self.xmlDoc, "PvPHelperForm", nil, self)
 		if self.wndMain == nil then
 			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
 			return
 		end
 		
-	    self.wndMain:Show(false, true)
+		self.wndMain:Show(false, true)
 
 		-- if the xmlDoc is no longer needed, you should set it to nil
 		-- self.xmlDoc = nil
@@ -102,25 +103,25 @@ function PvPHelper:OnDocLoaded()
 		Apollo.RegisterSlashCommand("pvphelper",            "OnPvPHelperOn", self)
 		Apollo.RegisterEventHandler("MatchEntered",         "OnPVPMatchEntered", self)
 		Apollo.RegisterEventHandler("MatchExited",          "OnPVPMatchExited", self)
-		Apollo.RegisterEventHandler("PvpRatingUpdated",     "OnPVPRatingUpdated", self)
-		Apollo.RegisterEventHandler("PVPMatchStateUpdated", "OnPVPMatchStateUpdated", self)	
+		-- Apollo.RegisterEventHandler("PvpRatingUpdated",     "OnPVPRatingUpdated", self)
+		-- Apollo.RegisterEventHandler("PVPMatchStateUpdated", "OnPVPMatchStateUpdated", self)	
 		Apollo.RegisterEventHandler("PVPMatchFinished",     "OnPVPMatchFinished", self)	
 		Apollo.RegisterEventHandler("PublicEventStart",     "OnPublicEventStart", self)
 
-		-- Do additional Addon initialization here
+		-- TODO: I feel that this could be done in a more elegant way, clean it up later
 		-- Maybe the UI reloaded so be sure to check if we are in a match already
 		if MatchingGame:IsInMatchingGame() then
 			local tMatchState = MatchingGame:GetPVPMatchState()
-	
+
 			if tMatchState ~= nil then
 				self:OnPVPMatchEntered()
 			end
-		end
-		
-		-- Do the same for public event
-		local tActiveEvents = PublicEvent.GetActiveEvents()
-		for idx, peEvent in pairs(tActiveEvents) do
-			self:OnPublicEventStart(peEvent)
+
+			-- Do the same for public event
+			local tActiveEvents = PublicEvent.GetActiveEvents()
+			for idx, peEvent in pairs(tActiveEvents) do
+				self:OnPublicEventStart(peEvent)
+			end
 		end
 	end
 end
@@ -136,18 +137,15 @@ function PvPHelper:OnPVPMatchEntered()
 		["sResult"]   = "N/A", 
 		["sRating"]   = "N/A"
 	}
-	
+
+	-- DEBUG
 	Event_FireGenericEvent("SendVarToRover", "OnPVPMatchEntered", self.currentMatch)
 end
 
 function PvPHelper:OnPVPMatchExited()
-end
-
-function PvPHelper:OnPVPRatingUpdated()
-end
-
-function PvPHelper:OnPVPMatchStateUpdated()
-	local result = MatchingGame
+	if self.currentMatch then
+		-- User left before match finished. TODO: Determine if incomplete data should be stored
+	end
 end
 
 function PvPHelper:OnPVPMatchFinished(eWinner, eReason, nDeltaTeam1, nDeltaTeam2)
@@ -156,13 +154,6 @@ function PvPHelper:OnPVPMatchFinished(eWinner, eReason, nDeltaTeam1, nDeltaTeam2
 	if tMatchState then
 		eMyTeam = tMatchState.eMyTeam
 	end	
-
-	--[[local result = {
-		["sDate"]     = PvPHelper:GetDateString(),
-		["sGameType"] = PvPHelper:GetGameTypeString(),
-		["sResult"]   = PvPHelper:GetResultString(eMyTeam, eWinner), 
-		["sRating"]   = PvPHelper:GetRatingString(tMatchState, { nDeltaTeam1, nDeltaTeam2 })
-	}]]--
 	
 	self.currentMatch["sResult"] = PvPHelper:GetResultString(eMyTeam, eWinner)
 	self.currentMatch["sRating"] = PvPHelper:GetRatingString(tMatchState, { nDeltaTeam1, nDeltaTeam2 })
@@ -180,12 +171,13 @@ function PvPHelper:OnPublicEventStart(peEvent)
 	local strType    = self:GetGameTypeString(eEventType)
 	
 	-- Only worry about PvP events
-	if strType == "N/A" then
+	if strType == "" then
 		return
 	end
 	
 	self.currentMatch["sGameType"] = strType
 	
+	-- DEBUG
 	Event_FireGenericEvent("SendVarToRover", "OnPublicEventStart", self.currentMatch)
 end
 
@@ -196,22 +188,6 @@ end
 
 -- on SlashCommand "/pvphelper"
 function PvPHelper:OnPvPHelperOn()
-	--[[local dummyData = {
-		{
-			["sDate"]     = "01/01/2001",
-			["sGameType"] = "Battleground",
-			["sResult"]   = "Win",
-			["sRating"]   = "+4 (1404)"
-		},
-		{
-			["sDate"]     = "01/01/2001",
-			["sGameType"] = "Battleground",
-			["sResult"]   = "Win",
-			["sRating"]   = "+4 (1404)"
-		},
-	}--]]
-
-	--Event_FireGenericEvent("SendVarToRover", "DummyData", dummyData)
 	
 	PvPHelper:HelperBuildGrid(self.wndMain:FindChild("GridContainer"), self.pvphelperdb.MatchHistory)
 	self.wndMain:Invoke() -- show the window
@@ -221,7 +197,6 @@ end
 function PvPHelper:OnPvPHelperClear()
 	Print("PvPHelper: Match History cleared")
 	self.pvphelperdb.MatchHistory = {}
-	return
 end
 
 -- DEPRECATED
@@ -247,7 +222,7 @@ end
 function PvPHelper:GetDateString()
 	local tDate = GameLib:GetLocalTime()
 	
-	local strDate = string.format("%d/%d/%d - %s", tDate["nMonth"], tDate["nMonth"], tDate["nYear"], tDate["strFormattedTime"])
+	local strDate = string.format("%d/%d/%d %s", tDate["nMonth"], tDate["nMonth"], tDate["nYear"], tDate["strFormattedTime"])
 	return strDate
 end
 
@@ -277,7 +252,7 @@ function PvPHelper:GetRatingString(tMatchState, tRatingDeltas)
 end
 
 function PvPHelper:GetGameTypeString(eEventType)
-	local result = "N/A"
+	local result = ""
 	
 	-- Leave these as if/elseif in case you want to add more specifics in the future
 	if eEventType == PublicEvent.PublicEventType_PVP_Battleground_HoldTheLine then
@@ -291,7 +266,7 @@ function PvPHelper:GetGameTypeString(eEventType)
 	elseif eEventType == PublicEvent.PublicEventType_PVP_Battleground_Sabotage then
 		result = "Battleground"
 	end
-	
+
 	return result
 end
 
@@ -311,29 +286,26 @@ end
 
 function PvPHelper:HelperBuildGrid(wndParent, tData)
 	if not tData then
-		Print("No data found")
+		-- Print("No data found")
 		return
 	end
 	
-	Print("Data found: building grid")
+	-- Print("Data found: building grid")
 
 	local wndGrid = wndParent:FindChild("ResultGrid")
 
 	local nVScrollPos 	= wndGrid:GetVScrollPos()
 	local nSortedColumn	= wndGrid:GetSortColumn() or 1
-	local bAscending 	= wndGrid:IsSortAscending()
+	local bAscending 	  = wndGrid:IsSortAscending()
 	
 	wndGrid:DeleteAll()
-
-	--local tMatchState 	= MatchingGame:GetPVPMatchState()
 	
 	for row, tMatch in pairs(tData) do
 		local wndResultGrid = wndGrid
-		--Event_FireGenericEvent("SendVarToRover", "Grid", wndResultGrid)
 		row = wndResultGrid:AddRow("")
+
 		for col, sDataKey in pairs(tDataKeys) do
 			local value = tMatch[sDataKey]
-			Print(string.format("setting %d, %d: %s", row, col, value))
 			wndResultGrid:SetCellText(row, col, value)
 		end
 	end
