542742b6d0a270f58d6a43d111f0c45192daf75f
orbv <orbv16@gmail.com>
2014-06-25 02:31:56 -0500

    Added Personal BG Rating tracking

--- PvPHelper.lua
+++ PvPHelper.lua
@@ -92,7 +92,7 @@ function PvPHelper:OnDocLoaded()
 		Apollo.RegisterSlashCommand("pvphelper",            "OnPvPHelperOn", self)
 		Apollo.RegisterEventHandler("MatchEntered",         "OnPVPMatchEntered", self)
 		Apollo.RegisterEventHandler("MatchExited",          "OnPVPMatchExited", self)
-		-- Apollo.RegisterEventHandler("PvpRatingUpdated",     "OnPVPRatingUpdated", self)
+		Apollo.RegisterEventHandler("PvpRatingUpdated",     "OnPVPRatingUpdated", self)
 		-- Apollo.RegisterEventHandler("PVPMatchStateUpdated", "OnPVPMatchStateUpdated", self)	
 		Apollo.RegisterEventHandler("PVPMatchFinished",     "OnPVPMatchFinished", self)	
 		Apollo.RegisterEventHandler("PublicEventStart",     "OnPublicEventStart", self)
@@ -103,6 +103,7 @@ function PvPHelper:OnDocLoaded()
 			local tMatchState = MatchingGame:GetPVPMatchState()
 
 			if tMatchState ~= nil then
+				--Print("Attempting to restore PVPMatchEntered()")
 				self:OnPVPMatchEntered()
 			end
 
@@ -122,41 +123,49 @@ end
 function PvPHelper:OnPVPMatchEntered()
 	local tDate = GameLib:GetLocalTime()
 	tDate["nTickCount"] = GameLib:GetTickCount()
+	local tRating = MatchingGame.GetPvpRating(MatchingGame.RatingType.RatedBattleground)
 
 	self.currentMatch = {
 		["tDate"]     = tDate,
 		["sGameType"] = "N/A",
 		["sResult"]   = "N/A", 
-		["sRating"]   = "N/A"
+		["sRating"]   = "N/A",
+		["nBGRating"] = tRating["nRating"]
 	}
-
-	-- DEBUG
-	--Event_FireGenericEvent("SendVarToRover", "OnPVPMatchEntered", self.currentMatch)
 end
 
 function PvPHelper:OnPVPMatchExited()
 	if self.currentMatch then
-		-- User left before match finished. TODO: Determine if incomplete data should be stored
-		self.currentMatch = {}
+		-- User left before match finished.
+		self.currentMatch["sResult"] = "Forfeit"
+		self:UpdateMatchHistory(self.currentMatch)
+	end
+end
+
+-- TODO: It would be better to update personal BG rating before this call is made.
+--       Possible if the rating change is passed via another event.
+function PvPHelper:OnPVPRatingUpdated(eType)
+	if eType == MatchingGame.RatingType["RatedBattleground"] then
+		self:UpdateBattlegroundRating()
 	end
 end
 
 function PvPHelper:OnPVPMatchFinished(eWinner, eReason, nDeltaTeam1, nDeltaTeam2)
 	local tMatchState = MatchingGame:GetPVPMatchState()
 	local eMyTeam = nil
+	local tRatingDeltas = {
+		nDeltaTeam1,
+		nDeltaTeam2
+	}
+	
 	if tMatchState then
 		eMyTeam = tMatchState.eMyTeam
 	end	
 	
-	self.currentMatch["sResult"] = PvPHelper:GetResultString(eMyTeam, eWinner)
-	self.currentMatch["sRating"] = PvPHelper:GetRatingString(tMatchState, { nDeltaTeam1, nDeltaTeam2 })
+	self.currentMatch["sResult"] = self:GetResultString(eMyTeam, eWinner)
+	self.currentMatch["sRating"] = self:GetArenaRatingString(tMatchState, tRatingDeltas)
 
-	if self.pvphelperdb.MatchHistory == nil then
-		self.pvphelperdb.MatchHistory = {}
-	end
-	table.insert(self.pvphelperdb.MatchHistory, self.currentMatch)
-	
-	self.currentMatch = nil
+	self:UpdateMatchHistory(self.currentMatch)
 end
 
 function PvPHelper:OnPublicEventStart(peEvent)
@@ -169,9 +178,6 @@ function PvPHelper:OnPublicEventStart(peEvent)
 	end
 	
 	self.currentMatch["sGameType"] = strType
-	
-	-- DEBUG
-	--Event_FireGenericEvent("SendVarToRover", "OnPublicEventStart", self.currentMatch)
 end
 
 -----------------------------------------------------------------------------------------------
@@ -192,6 +198,18 @@ function PvPHelper:OnPvPHelperClear()
 	self.pvphelperdb.MatchHistory = {}
 end
 
+function PvPHelper:UpdateBattlegroundRating()
+	if not self.pvphelperdb.MatchHistory then
+		return
+	end
+
+	local nLastEntry = #self.pvphelperdb.MatchHistory
+	local tLastEntry = self.pvphelperdb.MatchHistory[nLastEntry]
+	local nRating    = tLastEntry["nBGRating"]
+	
+	tLastEntry["sRating"] = self:GetBattlegroundRatingString(nRating)
+end
+
 function PvPHelper:GetDateString(tDate)	
 	local strDate = string.format("%02d/%02d/%4d %s", tDate["nMonth"], tDate["nDay"], tDate["nYear"], tDate["strFormattedTime"])
 	return strDate
@@ -205,13 +223,28 @@ function PvPHelper:GetResultString(eMyTeam, eWinner)
 	end
 end
 
--- Return a string which shows the current rating after difference
-function PvPHelper:GetRatingString(tMatchState, tRatingDeltas)
+function PvPHelper:GetBattlegroundRatingString(nPreviousRating)
 	local result = "N/A (N/A)"
+	local currentRating = MatchingGame.GetPvpRating(MatchingGame.RatingType.RatedBattleground)
 	
-	if tMatchState.arTeams and nDeltaTeam1 and nDeltaTeam2 then
-		local eMyTeam = tMatchState.eMyTeam	
+	if nPreviousRating and currentRating then
+		currentRating  = currentRating["nRating"]
+		if nPreviousRating < currentRating then
+			result = string.format("%d (+%d)", currentRating, (currentRating - nPreviousRating))
+		elseif nPreviousRating > currentRating then
+			result = string.format("%d (-%d)", currentRating, (nPreviousRating - currentRating))
+		end
+	end
+	 
+	return result
+end
 
+-- Return a string which shows the current rating after difference
+function PvPHelper:GetArenaRatingString(tMatchState, tRatingDeltas)
+	local eMyTeam = tMatchState.eMyTeam	
+	local result  = "N/A (N/A)"
+
+	if tMatchState.arTeams then
 		for idx, tCurr in pairs(tMatchState.arTeams) do
 			if eMyTeam == tCurr.nTeam then
 				result = string.format("%d (%d)", tCurr.nRating, tRatingDeltas[idx])
@@ -241,6 +274,14 @@ function PvPHelper:GetGameTypeString(eEventType)
 	return result
 end
 
+function PvPHelper:UpdateMatchHistory(tMatch)
+	if self.pvphelperdb.MatchHistory == nil then
+		self.pvphelperdb.MatchHistory = {}
+	end
+	table.insert(self.pvphelperdb.MatchHistory, tMatch)
+	
+	tMatch = nil
+end
 
 -----------------------------------------------------------------------------------------------
 -- PvPHelperForm Functions
